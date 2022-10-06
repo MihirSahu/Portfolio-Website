ca3817f08dd628e5f3538b0699a3031c650f13a0
Mihir Sahu <2002mihir@gmail.com>
2022-10-06 01:55:49 -0500

    Added IonIcons

--- portfolio-website/pages/index.tsx
+++ portfolio-website/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { IonIcon } from '@ionic/react';
 
 const Home: NextPage = () => {
   return (
@@ -62,6 +63,7 @@ const Home: NextPage = () => {
           Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <IonIcon icon={logo-linkedin} />
           </span>
         </a>
       </footer>
