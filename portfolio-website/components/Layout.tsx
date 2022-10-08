3bd74a4f23d522862c1aa3a40fc6a685fb5909cf
Mihir Sahu <2002mihir@gmail.com>
2022-10-08 12:38:36 -0500

    Fixed h-screen bug in Layout.tsx

--- portfolio-website/components/Layout.tsx
+++ portfolio-website/components/Layout.tsx
@@ -1,6 +1,6 @@
 const Layout = (props: any) => {
     return (
-        <div className = "h-screen flex">
+        <div className = "flex">
             <div className = "max-w-3xl m-auto">{props.children}</div>
         </div>
     );
