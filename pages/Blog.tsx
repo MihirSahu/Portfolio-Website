57413c1a39348e65d3c1992c7c744c5dbb62db7c
Mihir Sahu <2002mihir@gmail.com>
2022-10-21 02:48:15 -0500

    Update Blog.tsx
    Automatically populate blog page with cards for each markdown file present in the blog/ directory

--- pages/Blog.tsx
+++ pages/Blog.tsx
@@ -1,26 +1,63 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import fs from 'fs';
 
 
-const Blog = () => {
+const BlogCard = (article: string) => {
 
-    return(
+    //console.log(article);
+    return (
         <>
             <div className={styles.grid}>
-                <Link href="/blog/Corridor">
+                {/* The below code automatically creates a card with a link for each markdown file passed to this component from Blog.getInitialProps */}
+                <Link href={"./blog/" + article?.article.substr(0, article?.article.indexOf('.'))}>
                     <a
                         target="_blank"
                         className={styles.longCard}
                     >
-                        <h2>TryHackMe Corridor CTF Writeup &rarr;</h2>
+                        <h2>TryHackMe {article?.article.substr(0, article?.article.indexOf('.'))} CTF Writeup &rarr;</h2>
                         <p>
-                            My writeup for John Hammond's Corridor CTF.
+                            My writeup for {article?.article.substr(0, article?.article.indexOf('.'))} CTF.
                         </p>
                     </a>
                 </Link>
             </div>
         </>
     );
+}
+
+const Blog = ({ articles }: { articles: string[] }) => {
+
+    //console.log(articles);
+    const blogCards = [];
+
+    for (let article of articles) {
+        blogCards.push(<BlogCard article={article} />);
+    }
+
+    return (
+        <>
+            {blogCards}
+        </>
+    );
+}
+
+Blog.getInitialProps = () => {
+
+    const articlePath = './pages/blog/';
+    const articleList: string[] = [];
+
+    fs.readdirSync(articlePath).forEach(file => {
+        if (file.slice(-3) === ".md") {
+                //console.log(file);
+                articleList.push(file);
+            }
+        }
+    );
+
+    //console.log(articleList);
+
+    return { articles: articleList };
 }
 
 export default Blog;
