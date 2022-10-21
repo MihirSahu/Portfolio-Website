d9ee91e5fb52b1d2645a1809b528da78285d37d0
Mihir Sahu <2002mihir@gmail.com>
2022-10-21 02:47:14 -0500

    Create Git_Happens.tsx

---
+++ pages/blog/Git_Happens.tsx
@@ -0,0 +1,11 @@
+import BlogPost from "../../components/BlogPost";
+// @ts-ignore
+import Git_Happens from '../blog/Git_Happens.md';
+
+const GitHappens = () => {
+    return (
+        <BlogPost blogPost={Git_Happens} />
+    );
+}
+
+export default GitHappens;
