3c1adc474ae976c42ce5a35ca713aa9abd948236
Mihir Sahu <46336107+MihirSahu@users.noreply.github.com>
2022-11-16 13:48:50 -0600

    Add buttons to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,9 +14,10 @@ const Header = () => {
             <button onClick={() => router.back()}><BackButton className="text-3xl pl-1 pr-1" height={30} width={30} fill={'white'} /></button>
           </div>
           <div className='flex flex-row space-x-4'>
+            <Link href="/"><a className={styles.resumeButton}>Home</a></Link>
             <Link href="/About"><a className={styles.resumeButton}>About</a></Link>
-            <Link href="/About"><a className={styles.resumeButton}>Experience</a></Link>
-            <Link href="/About"><a className={styles.resumeButton}>Projects</a></Link>
+            <Link href="/Experience"><a className={styles.resumeButton}>Experience</a></Link>
+            <Link href="/Projects"><a className={styles.resumeButton}>Projects</a></Link>
           </div>
         </header>
       </>
